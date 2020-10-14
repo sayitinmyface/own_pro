d7bd55c0fdd47ac73dea65421dae63a61c01a11c
sayitinmyface <leo99roo@gmail.com>
2020-10-14 11:53:43 +0900

    10141153

--- scraptingjobinfo.py
+++ scraptingjobinfo.py
@@ -7,50 +7,76 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import datetime
 import time
+
+    
 # import os
 
 def scrapingjobinfo():
-    db_url = 'mongodb://192.168.219.105:27017'
+    db_url = 'mongodb://192.168.219.116:27017'
     url = 'https://www.k-startup.go.kr/common/announcement/announcementList.do?mid=30004&bid=701&searchAppAt=A'
-    #     
     driver = webdriver.Chrome(executable_path='/home/hanyohan/Documents/Develop/chromedriver')
     driver.get(url)
-    
-    s_ul = driver.find_element_by_class_name('ann_list')
-    s_li = s_ul.find_elements_by_tag_name('li')
     # 
-    print()
+    s_li = driver.find_elements(By.CSS_SELECTOR,'[id*="liArea"]')
+    # 
     with MongoClient(db_url) as client:
         mydb = client['mydb']
+        try:
+            while not driver.find_element_by_class_name('btn_listAll') is None:
+                driver.find_element_by_class_name('btn_listAll').click()
+        except:
+            driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
+        # 
+        # time.sleep(3)
         for fs_li in s_li:            
-            fs_li.find_element_by_tag_name('button').click()            
-            driver.switch_to_window(driver.window_handles[-1])
-            html = driver.page_source
-            soup = BeautifulSoup(html,'lxml')
-            div = soup.select('div[class*="type_text"]')
-            # 
-            post_title = soup.select_one('h4').string
-            term = div[0].string
-            accept = div[1].stirng
-            target = div[2].stirng
-            schedule = div[3].stirng
-            spot = div[4].stirng
-            content = div[5].stirng
-            # 
-            data = {
-                    'title':post_title,
-                    'term':term,
-                    'accept':accept,
-                    'target':target,
-                    'schedule':schedule,
-                    'spot':spot,
-                    'content':content,
-                    'create_date':datetime.datetime.now()
-            }
-            # 
-            infor = mydb.startupinfo.insert_one(data)     
-            driver.close()
-            driver.switch_to_window(driver.window_handles[0])
-            time.sleep(10)
-
+            if fs_li.find_element_by_class_name('ann_list_group05 ').text != '시설·공간·보육':
+                fs_li.find_element_by_class_name('bt_Nwindow').click()                        
+                driver.switch_to_window(driver.window_handles[-1])
+                # 
+                html = driver.page_source
+                soup = BeautifulSoup(html,'lxml')
+                div = soup.select('div[class*="type_text"]')
+                tbody = soup.select_one('tbody')
+                td = tbody.select('td')
+                # 
+                str_content = ''
+                for f_div in div:
+                    str_content += str(f_div.string)
+                str_content = str_content.replace('None','')
+                str_content = str_content.replace('\t','')    
+                # 
+                post_title = soup.select_one('h4').string
+                organization = td[0].string.strip()
+                organization_sub = td[1].string.strip()
+                department = td[2].string.strip()
+                contact_address = td[3].string.strip()
+                term = td[4].string.strip()
+                support = td[5].string.strip()
+                local = td[6].string.strip()
+                period = td[7].string.strip()
+                subject = td[8].string.strip()
+                age = td[9].string.strip()
+                content = str_content
+                # 
+                data = {
+                        'post_title':post_title,
+                        'organization':organization,
+                        'organization_sub':organization_sub,
+                        'department':department,
+                        'contact_address':contact_address,
+                        'term':term,
+                        'support':support,
+                        'local':local,
+                        'period':period,
+                        'subject':subject,
+                        'age':age,
+                        'content':content,
+                        'create_date':datetime.datetime.now()
+                }
+                # 
+                infor = mydb.startupinfo.insert_one(data)     
+                driver.close()
+                driver.switch_to_window(driver.window_handles[0])
+                time.sleep(2)#
+# 
 scrapingjobinfo()        
